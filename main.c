fe1fdb7d739f8abea8c18d130f9a4af3b8ec935b
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-02-04 23:52:51 +0530

    fourth commit

--- main.c
+++ main.c
@@ -31,24 +31,93 @@ void listTasks(){
     status = 'n';
   }
 
-}
+  printf("%d. %s [%c]\n", i + 1, tasks[i].task, status);
 
 }
 
-void markCompleted(){
-
 }
 
-void deleteTasks(){
+void markCompleted(int index){
+  if(index <= length && index > 0){
+    tasks[index - 1].completed = 1;
+    printf("Task marked as completed\n");
 
+  } else{
+    printf("Invalid index\n");
+  }
 }
 
-void editTask(){
+void deleteTasks(int index){
+  if(index <= length && index > 0){
+      index = index -1;
 
+      free(tasks[index].task);
+
+      for (int i = index; i < length - 1; i++){
+          tasks[i] = tasks[i + 1];
+      }
+
+      length--;
+
+      tasks = (Task *)realloc(tasks, (length * sizeof(Task)));
+  } else{
+    printf("Invalid index\n");
+  }
+}
+
+void editTask(int index, const char* task){
+  if(index <= length && index > 0){
+      index = index - 1;
+
+      char *editedTask = (char *)realloc(tasks[index].task, strlen(task) + 1);
+    if (editedTask != NULL){
+      tasks[index].task = editedTask;
+        strcpy(tasks[length].task, task);
+        printf("Task Updated Successfully\n");
+    } else{
+
+      printf("Memory Allocation Failed\n");
+    }
+
+
+  } else {
+    printf("Invalid index\n");
+  }
 }
 
 
 
 int main(){
+
+  int running = 1;
+
+  printf("\nOptions\n");
+  printf("1. Add a Task\n");
+  printf("2. List All Tasks\n");
+  printf("3. Mark as Complete\n");
+  printf("4. Edit Task\n");
+  printf("5. Delete Task\n");
+  printf("6. Exit\n");
+
+  while (running) {
+    printf("Enter choice(1,2,3,4,5,6):");
+    scanf("%d", &choice);
+
+    switch (choice) {
+      case 1:
+        printf("Enter Task:");
+        getchar
+      case 2;
+      case 3;
+      case 4;
+      case 5;
+      case 6;
+
+      default:
+        printf("Invalid Choice!!!\n");
+        break;
+    }
+  }
+
   return 0;
 }
