1266cc04707d373737d16d1be39ba53c93105faa
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-02-04 12:17:14 +0530

    third

--- main.c
+++ main.c
@@ -11,12 +11,27 @@ Task *tasks = NULL;
 int length = 0;
 
 void addTask(const char *task){
+  tasks = (Task *)realloc(tasks, (length + 1) * sizeof(Task));
+  tasks[length].task = (char *)malloc(strlen(task) + 1);
+  tasks[length].completed = 0;
 
+  strcpy(tasks[length].task, task);
 
+  length++;
+  printf("Task Added");
 }
 
 void listTasks(){
+  char status;
 
+  for (int i = 0; i < length; i++){
+    if(tasks[i].completed == 1){
+    status = 'd';
+  } else {
+    status = 'n';
+  }
+
+}
 
 }
 
